66f320761aa917bf42131a2b77b9b7363c283e6c
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-02 16:35:30 +0900

    solve

--- past202203_h.py
+++ past202203_h.py
@@ -1,33 +1,54 @@
-def modpow(a, b, m):
-    ret = 1
-    k = a
-    while b > 0:
-        if b & 1:
-            ret = ret * k % m
-        k = k**2 % m
-        b //= 2
+from collections import deque
 
-    return ret
+class UnionFind:
+    def __init__(self, n):
+        self.n = n
+        # 正の数の場合は親のインデックス
+        # 負の数の場合は木のサイズ
+        self.parent_or_size = [-1] * n
+    
+    def merge(self, a, b):
+        x = self.leader(a)
+        y = self.leader(b)
+        if x == y: return x
 
-def comb(n, r, m):
-    a = fact[n]
-    b = fact[r] * fact[n - r] % m
-    return a * modpow(b, m - 2, m) % m
+        if -self.parent_or_size[x] < -self.parent_or_size[y]:
+            x, y = y, x
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
 
-N, K = map(int, input().split())
-M = 998244353
-A = list(map(int, input().split()))
-A.sort(reverse=True)
+        return x
 
-fact = [0] * (N+1)
-fact[0] = 1
-for i in range(1, N+1):
-    fact[i] = i * fact[i - 1] % M
+    def same(self, a, b):
+        return self.leader(a) == self.leader(b)
 
-a = 0
-j = 0
-for i in range(N-1, K-2, -1):
-    a = (a + A[j]*comb(i, K-1, M) - A[N-1-j]*comb(i, K-1, M)) % M
-    j += 1
+    def leader(self, a):
+        if self.parent_or_size[a] < 0: return a
 
-print(a * fact[K] * fact[N-K] * modpow(fact[N], M-2, M) % M)
+        # 効率化のための根の付け替え
+        self.parent_or_size[a] = self.leader(self.parent_or_size[a])
+        return self.parent_or_size[a]
+
+N, Q = map(int, input().split())
+uf = UnionFind(N)
+G = [[] for _ in range(N)]
+for _ in range(Q):
+    query = tuple(map(lambda x: int(x)-1, input().split()))
+
+    if query[0] == 0:
+        t, u, v = query
+        if not uf.same(u, v):
+            uf.merge(u, v)
+            G[u].append(v)
+            G[v].append(u)
+    else:
+        t, u = query
+        que = deque([(u, -1)])
+        res = [u]
+        while que:
+            cur, pre = que.popleft()
+            for gi in G[cur]:
+                if gi == pre: continue
+                que.append((gi, cur))
+                res.append(gi)
+        print(*sorted(map(lambda x: x+1, res)))
