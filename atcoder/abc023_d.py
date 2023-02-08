ba2b7dcb0891464576d275df106c69336e68fe03
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-09 00:06:13 +0900

    solve

--- atcoder/abc023_d.py
+++ atcoder/abc023_d.py
@@ -1,38 +1,19 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-
-# Hi + Si * N <= X
-# Si * N <= X - Hi
-# N <= (X - Hi)/Si
 def check(X):
-    A = [0] * N
+    a = []
+    for h, s in baloon:
+        if X < h: return False
+        a.append((X-h)//s)
+    a.sort()
     for i in range(N):
-        if X < H[i]: return False
-
-        A[i] = ((X - H[i])//S[i], H[i], S[i])
-    A.sort()
-    mx = 0
-
-    for i in range(N):
-        t, h, s = A[i]
-        if i > t: return False
-
-        mx = max(mx, h + s * i)
-        if mx > X: return False
+        if i > a[i]: return False
 
     return True
 
 N = int(input())
-H = [0] * N
-S = [0] * N
-M = 10**9
-for i in range(N):
-    h, s = map(int, input().split())
-    H[i] = h
-    S[i] = s
+baloon = [list(map(int, input().split())) for _ in range(N)]
 
 ng = 0
-ok = M*N
+ok = 10**18
 while abs(ok - ng) > 1:
     mid = (ok + ng) // 2
     if check(mid):
