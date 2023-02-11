4192b6b537f17a61e8ee6f9289831562a08e08f0
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-11 23:08:47 +0900

    想定解法

--- atcoder/abc289_b.py
+++ atcoder/abc289_b.py
@@ -1,31 +1,20 @@
-def rev(a, s, e):
-    while s < e:
-        a[s], a[e] = a[e], a[s]
-        s += 1
-        e -= 1
-
 N, M = map(int, input().split())
 a = list(map(int, input().split()))
-ans = list(range(1, N+1))
 
-if M == 0:
-    print(*ans)
-    exit()
-
-s = a[0]
-mae = a[0]
-for i in range(1, M):
-    if mae + 1 == a[i]:
-        mae = a[i]
-    else:
-        # sからa[i]+1を逆順にする
-        # print("s:", s, "a[i]+1:", a[i]+1)
-        # print(*ans)
-        rev(ans, ans.index(s), ans.index(mae+1))
-        # print(*ans)
-        s = a[i]
-        mae = a[i]
-
-rev(ans, ans.index(s), ans.index(mae+1))
+re = [False] * (N+1)
+for ai in a:
+    re[ai] = True
+ans = []
+i = 1
+j = 1
+while i <= N:
+    while re[j]:
+        j += 1
+    k = j
+    while k >= i:
+        ans.append(k)
+        k -= 1
+    j += 1
+    i = j
 
 print(*ans)
