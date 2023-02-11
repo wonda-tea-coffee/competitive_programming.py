3f92d1d897c3602959ead988118c124590aafce9
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-11 15:09:57 +0900

    iranu

--- atcoder/past202012_e.py
+++ atcoder/past202012_e.py
@@ -39,30 +39,24 @@ def rotate(hanko):
 def search(hanko, dodai):
     dx = len(dodai) - len(hanko)
     dy = len(dodai[0]) - len(hanko[0])
-    for x in range(0, dx+1):
-        for y in range(0, dy+1):
+    for x in range(dx+1):
+        for y in range(dy+1):
             if (stamp(hanko, dodai, x, y)):
                 return True
     return False
 
 def stamp(hanko, dodai, xpos, ypos):
-    for x in range(0, len(hanko)):
-        for y in range(0, len(hanko[0])):
+    for x in range(len(hanko)):
+        for y in range(len(hanko[0])):
             c1 = hanko[x][y]
             c2 = dodai[x+xpos][y+ypos]
             if (c1 == '#' and c2 == '#'):
                 return False
     return True
 
-print("init:", hanko)
-
 hanko = shrink(hanko)
 
-print("shrink:", hanko)
-
 for _ in range(4):
-    print("rotate:", hanko)
-
     if search(hanko, dodai):
         print("Yes")
         exit()
