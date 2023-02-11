8bad2173d26cd23ca559a6b30aa9d6a061300894
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-11 23:33:55 +0900

    solve

--- atcoder/abc289_e.py
+++ atcoder/abc289_e.py
@@ -1,30 +1,24 @@
-from collections import deque
-import sys
-input = lambda: sys.stdin.readline().rstrip()
+from collections import *
 
 T = int(input())
-
 for _ in range(T):
     N, M = map(int, input().split())
     C = list(map(int, input().split()))
+
     G = [[] for _ in range(N)]
     for _ in range(M):
         u, v = map(lambda x: int(x)-1, input().split())
-        G[u].append(v)
-        G[v].append(u)
+        G[u] += [v]
+        G[v] += [u]
 
-    q = deque([(0, N-1, set([(0, N-1)]), 0)])
-    res = False
-    while q:
-        curt, cura, state, step = q.popleft()
-        if curt == N-1 and cura == 0:
-            res = True
-            print(step)
-            break
-        for tot in G[curt]:
-            for toa in G[cura]:
-                if (tot, toa) in state: continue
-                if C[tot] == C[toa]: continue
-                q.append((tot, toa, state|{(tot, toa)}, step+1))
-    if not res:
-        print(-1)
+    D = [[-1] * N for _ in range(N)]
+    D[0][-1] = 0
+    Q = deque([(0, N-1)])
+    while Q:
+        vt, va = Q.popleft()
+        for ct in G[vt]:
+            for ca in G[va]:
+                if C[ct] != C[ca] and D[ct][ca] < 0:
+                    D[ct][ca] = D[vt][va] + 1
+                    Q.append((ct, ca))
+    print(D[-1][0])
