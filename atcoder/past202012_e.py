bcebc93c8e2b2d86794144c3aa5e92b6caee092e
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-13 08:11:06 +0900

    ittan

--- atcoder/past202012_e.py
+++ atcoder/past202012_e.py
@@ -1,66 +1,51 @@
 H, W = map(int, input().split())
-dodai = [input() for _ in range(H)]
-hanko = [input() for _ in range(H)]
+S = []
+for _ in range(H):
+    S.append(list(input()))
+T = []
+for _ in range(H):
+    T.append(list(input()))
 
-# ハンコの余分な部分を取り除く
-def shrink(hanko):
-    newhanko = []
-    xmax = 0
-    xmin = 99
-    ymax = 0
-    ymin = 99
-    for y in range(H):
-        for x in range(W):
-            if (hanko[y][x] == '#'):
-                if (xmax < x): xmax = x
-                if (xmin > x): xmin = x
-                if (ymax < y): ymax = y
-                if (ymin > y): ymin = y
+def shrink(t):
+    miny = H
+    maxy = 0
+    minx = W
+    maxx = 0
+    for i in range(H):
+        for j in range(W):
+            if t[i][j] == "#":
+                miny = min(miny, i)
+                maxy = max(maxy, i)
+                minx = min(minx, j)
+                maxx = max(maxx, j)
 
-    for y in range(ymin, ymax+1):
-        s = ''
-        for x in range(xmin, xmax+1):
-            s += hanko[y][x]
-        newhanko.append(s)
-    return newhanko
+    ret = [[None]*(maxx-minx+1) for _ in range(maxy-miny+1)]
+    for i in range(maxy+1):
+        for j in range(maxx+1):
+            ret[i][j] = t[i+miny][j+minx]
+    return ret
 
-# ハンコを右に90度回す
-def rotate(hanko):
-    newhanko = []
-    height = len(hanko)
-    width = len(hanko[0])
-    for x in range(width):
-        s = ''
-        for y in range(height):
-            s = hanko[y][x] + s
-        newhanko.append(s)
-    return newhanko
+def rotate(t):
+    h = len(t)
+    w = len(t[0])
+    ret = [[None]*h for _ in range(w)]
 
-def search(hanko, dodai):
-    dy = len(dodai) - len(hanko)
-    dx = len(dodai[0]) - len(hanko[0])
-    for y in range(dy+1):
-        for x in range(dx+1):
-            if stamp(hanko, dodai, y, x):
-                return True
-    return False
+    for i in range(w):
+        for j in range(h):
+            ret[i][j] = t[j][w-i-1]
 
-# 土台の左上隅を決めてハンコを押せるかチェック
-def stamp(hanko, dodai, ypos, xpos):
-    for y in range(len(hanko)):
-        for x in range(len(hanko[0])):
-            c1 = hanko[y][x]
-            c2 = dodai[y+ypos][x+xpos]
-            if c1 == '#' and c2 == '#':
-                return False
-    return True
+    return ret
 
-hanko = shrink(hanko)
+def search(t):
+    pass
 
-for _ in range(4):
-    if search(hanko, dodai):
-        print("Yes")
-        exit()
-    hanko = rotate(hanko)
+def stamp(t):
+    pass
+
+T = shrink(T)
+for _ in range(3):
+    if search(T):
+        exit(print("Yes"))
+    T = rotate(T)
 
 print("No")
