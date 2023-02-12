19178dd27d166c682d5b03d1caf98c7666625100
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-12 21:12:19 +0900

    aaaaaaaaa

--- past202109_h.py
+++ past202109_h.py
@@ -7,27 +7,18 @@ for _ in range(N-1):
     G[a-1].append((b-1, c))
     G[b-1].append((a-1, c))
 
-minpath = [[10**10]*N for _ in range(N)]
-
 for i in range(N):
-    Q = deque([(i, 0)])
-    visited = set([i])
-    minpath[i][i] = 0
+    dist = [-1] * N
+    dist[i] = 0
+    Q = deque([i])
     while Q:
-        cur, length = Q.popleft()
-
+        cur = Q.pop()
         for to, cost in G[cur]:
-            if to in visited: continue
-            visited.add(to)
-            minpath[cur][to] = min(minpath[cur][to], cost)
-            minpath[i][to] = min(minpath[i][to], length+cost)
+            if dist[to] != -1: continue
+            dist[to] = dist[cur] + cost
+            Q.append(to)
 
-            Q.append((to, length+cost))
-
-for i in range(N):
-    for j in range(N):
-        if minpath[i][j] == X:
-            print("Yes")
-            exit()
+    if X in dist:
+        exit(print("Yes"))
 
 print("No")
