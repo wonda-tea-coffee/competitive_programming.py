b6ca1f40120a95ad739def8058895a3fcff9bf76
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-13 11:39:10 +0900

    mata toita

--- atcoder/past202109_g.py
+++ atcoder/past202109_g.py
@@ -1,24 +1,24 @@
 def is_ok(x):
-    c = 0
+    cnt = 0
+
     for i in range(N):
         if x < B[i]: continue
-        k = (x - B[i]) // C[i] + 1
-        c += min(k, A[i])
+        # B[i]+C[i]*s <= x
+        # s <= (x - B[i])/C[i]
+        cnt += min((x - B[i])//C[i] + 1, A[i])
 
-    return c >= K
+    return cnt >= K
 
 N, K = map(int, input().split())
-A = []
-B = []
-C = []
+A, B, C = [], [], []
 for _ in range(N):
     a, b, c = map(int, input().split())
     A.append(a)
     B.append(b)
     C.append(c)
 
-ok = max(B) + max(A)*max(C)
 ng = 0
+ok = 10**30
 while abs(ok - ng) > 1:
     mid = (ok + ng) // 2
     if is_ok(mid):
