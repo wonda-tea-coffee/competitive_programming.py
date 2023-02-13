28cfd8454b99084834deb39b9992adef073a7617
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-13 22:15:29 +0900

    460ms
    https://atcoder.jp/contests/abc289/submissions/38895233

--- atcoder/abc289_e.py
+++ atcoder/abc289_e.py
@@ -1,24 +1,30 @@
-from collections import *
+from collections import deque
 
 T = int(input())
 for _ in range(T):
     N, M = map(int, input().split())
     C = list(map(int, input().split()))
-
-    G = [[] for _ in range(N)]
+    E = [[] for _ in range(N)]
     for _ in range(M):
         u, v = map(lambda x: int(x)-1, input().split())
-        G[u] += [v]
-        G[v] += [u]
+        E[u].append(v)
+        E[v].append(u)
 
-    D = [[-1] * N for _ in range(N)]
+    Q = deque([(0, N-1, 0)])
+    ans = -1
+    D = [[-1]*N for _ in range(N)]
     D[0][-1] = 0
-    Q = deque([(0, N-1)])
     while Q:
-        vt, va = Q.popleft()
-        for ct in G[vt]:
-            for ca in G[va]:
-                if C[ct] != C[ca] and D[ct][ca] < 0:
-                    D[ct][ca] = D[vt][va] + 1
-                    Q.append((ct, ca))
+        t, a, step = Q.popleft()
+
+        if t == N-1 and a == 0:
+            break
+
+        for tot in E[t]:
+            for toa in E[a]:
+                if D[tot][toa] >= 0: continue
+                if C[tot] == C[toa]: continue
+                D[tot][toa] = D[t][a] + 1
+                Q.append((tot, toa, step+1))
+
     print(D[-1][0])
