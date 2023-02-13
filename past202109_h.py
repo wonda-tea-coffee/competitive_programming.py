de0f7655ad1d5f04b0c2b179a898659a21237ab6
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-14 00:40:18 +0900

    solve

--- past202109_h.py
+++ past202109_h.py
@@ -1,24 +1,25 @@
 from collections import deque
 
 N, X = map(int, input().split())
-G = [[] for _ in range(N)]
+edges = [[] for _ in range(N)]
 for _ in range(N-1):
     a, b, c = map(int, input().split())
-    G[a-1].append((b-1, c))
-    G[b-1].append((a-1, c))
+    a, b = a-1, b-1
+    edges[a].append((b, c))
+    edges[b].append((a, c))
 
 for i in range(N):
     dist = [-1] * N
     dist[i] = 0
-    Q = deque([i])
-    while Q:
-        cur = Q.pop()
-        for to, cost in G[cur]:
-            if dist[to] != -1: continue
-            dist[to] = dist[cur] + cost
-            Q.append(to)
-
+    q = deque([i])
+    while q:
+        x = q.pop()
+        for y, cost in edges[x]:
+            if dist[y] != -1: continue
+            dist[y] = dist[x] + cost
+            q.append(y)
     if X in dist:
-        exit(print("Yes"))
+        print("Yes")
+        exit()
 
 print("No")
