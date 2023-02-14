102ef4a8538c17a34a36228a312f4d4a200d059a
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-14 22:18:07 +0900

    solve

---
+++ aoj/DPL_1_B.py
@@ -0,0 +1,21 @@
+import copy
+
+N, W = map(int, input().split())
+v = []
+w = []
+for _ in range(N):
+    vi, wi = map(int, input().split())
+    v.append(vi)
+    w.append(wi)
+
+dp = [0]*(W+1)
+for i in range(N):
+    ndp = copy.deepcopy(dp)
+
+    for j in range(W+1):
+        if j + w[i] <= W:
+            ndp[j + w[i]] = max(ndp[j + w[i]], dp[j] + v[i])
+
+    dp = ndp
+
+print(max(dp))
