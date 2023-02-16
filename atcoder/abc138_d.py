cbd121f3af20cf55e8dd680bb1e36991d9a83125
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-17 01:59:58 +0900

    solve

--- atcoder/abc138_d.py
+++ atcoder/abc138_d.py
@@ -1,14 +1,14 @@
 import sys
 sys.setrecursionlimit(1000000)
-input = lambda: sys.stdin.readline().rstrip()
 
 N, Q = map(int, input().split())
-edge = [[] for _ in range(N)]
+E = [[] for _ in range(N)]
 for _ in range(N-1):
     a, b = map(lambda x: int(x)-1, input().split())
-    edge[a].append(b)
-    edge[b].append(a)
+    E[a].append(b)
+    E[b].append(a)
 cnt = [0]*N
+
 for _ in range(Q):
     p, x = map(int, input().split())
     cnt[p-1] += x
@@ -16,7 +16,7 @@ for _ in range(Q):
 done = [False]*N
 done[0] = True
 def dfs(n):
-    for i in edge[n]:
+    for i in E[n]:
         if done[i]: continue
         done[i] = True
         cnt[i] += cnt[n]
