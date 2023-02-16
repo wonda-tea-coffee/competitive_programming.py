3a5ea07c84ef7d4c4fddb3e41f300aa6259907f6
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-17 03:03:00 +0900

    toita

--- atcoder/joi2013yo_d.py
+++ atcoder/joi2013yo_d.py
@@ -8,16 +8,11 @@ for _ in range(N):
     A.append(a)
     B.append(b)
     C.append(c)
-# dp[i][j] := i日目までで、最後に服jを選んだ場合の最大値
-dp = [[0]*N for _ in range(D)]
+dp = [[-10**10]*N for _ in range(D)]
 for i in range(N):
     if A[i] <= T[0] <= B[i]:
-        pass
-    else:
-        dp[0][i] = -10000
-
+        dp[0][i] = 0
 for i in range(1, D):
-    # i日目に着られる服の候補
     cs = []
     for j in range(N):
         if A[j] <= T[i] <= B[j]:
@@ -25,6 +20,5 @@ for i in range(1, D):
     for csi in cs:
         for j in range(N):
             dp[i][csi] = max(dp[i][csi], dp[i-1][j] + abs(C[j] - C[csi]))
-    # print("i=", i, "cs=", cs, "dp=", dp)
 
 print(max(dp[-1]))
