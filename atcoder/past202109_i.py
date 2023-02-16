99025ef6be2f62e59d5f45bca39f81ff0c9d7155
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-17 00:59:32 +0900

    mata toita

--- atcoder/past202109_i.py
+++ atcoder/past202109_i.py
@@ -2,17 +2,15 @@ import heapq
 
 N = int(input())
 A = list(map(int, input().split()))
-
 k = 0
-odd = []
 for i in range(N):
     while A[i] % 2 == 0:
         A[i] //= 2
         k += 1
 
-    heapq.heappush(odd, A[i])
-
+heapq.heapify(A)
 for i in range(k):
-    temp = heapq.heappop(odd)
-    heapq.heappush(odd, 3*temp)
-print(heapq.heappop(odd))
+    t = heapq.heappop(A)
+    heapq.heappush(A, t*3)
+
+print(heapq.heappop(A))
