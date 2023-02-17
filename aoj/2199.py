cfefef9058f1c7ce5f09bd6e861a544b2e4a3ecf
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-17 17:31:54 +0900

    AC

--- aoj/2199.py
+++ aoj/2199.py
@@ -1,5 +1,13 @@
 INF = float("inf")
 
+def round255(n):
+	if n > 255:
+		return 255
+	elif n < 0:
+		return 0
+	else:
+		return n
+
 while True:
 	N, M = map(int, input().split())
 	if N == M == 0: break
@@ -7,21 +15,13 @@ while True:
 	C = tuple(int(input()) for _ in range(M))
 	X = tuple(int(input()) for _ in range(N))
 
-	p = set()
-	for j in range(256):
-		for c in C:
-			if j+c < 0:
-				to = 0
-			elif j+c > 255:
-				to = 255
-			else:
-				to = j+c
-			p.add((to, j))
 	dp = [INF]*256
 	dp[128] = 0
 	for x in X:
 		dp2 = list(dp)
 		dp = [INF]*256
-		for to, j in p:
-			dp[to] = min(dp[to], dp2[j] + (x-to)**2)
+		for j in range(256):
+			for c in C:
+				to = round255(j+c)
+				dp[to] = min(dp[to], dp2[j] + (x-to)**2)
 	print(min(dp))
