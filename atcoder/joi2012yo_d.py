afbd9a4eb7794d6543d2fd31a2613b796d6fbd56
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-18 07:42:26 +0900

    hai

--- atcoder/joi2012yo_d.py
+++ atcoder/joi2012yo_d.py
@@ -4,7 +4,7 @@ F = [-1]*N
 for _ in range(K):
     a, b = map(lambda x: int(x)-1, input().split())
     F[a] = b
-dp = [[[0]*3 for _ in range(3)] for _ in range(101)]
+dp = [[[0]*3 for _ in range(3)] for _ in range(N+1)]
 dp[0][0][0] = 1
 
 for i in range(N):
@@ -15,12 +15,13 @@ for i in range(N):
     for a in range(3):
         for b in range(3):
             for c in cs:
-                if i >= 2 and a == b and b == c: continue
+                if i >= 2 and a == b == c: continue
                 dp[i+1][b][c] += dp[i][a][b]
                 dp[i+1][b][c] %= MOD
 
 ans = 0
 for a in range(3):
     for b in range(3):
-        ans = (ans + dp[N][a][b]) % MOD
+        ans += dp[N][a][b]
+        ans %= MOD
 print(ans)
