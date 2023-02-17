c76c9996a8ca72abc5bb2825999a8209a6449efb
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-17 19:36:23 +0900

    AC

--- aoj/ALDS1_10_B.py
+++ aoj/ALDS1_10_B.py
@@ -6,10 +6,13 @@ dp = [[float("inf")]*N for _ in range(N)]
 for i in range(N):
     dp[i][i] = 0
 
-for le in range(1, N):
-    for i in range(N-le):
+# 長さ
+for length in range(1, N):
+    # 左端
+    for i in range(N-length):
+        # 右端
         j = i + le
         for k in range(i, j):
-            dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + M[i][0]*M[k][1]*M[j][1])
+            dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + M[i][0] * M[k][1] * M[j][1])
 
 print(dp[0][-1])
