c42832889b896f8d1589149e9b1d4a99836f2d27
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-18 08:02:34 +0900

    solve

--- atcoder/abc023_d.py
+++ atcoder/abc023_d.py
@@ -1,24 +1,27 @@
-def check(X):
+def is_ok(x):
     a = []
-    for h, s in baloon:
-        if X < h: return False
-        a.append((X-h)//s)
+    for i in range(N):
+        if x < H[i]: return False
+        a.append((x-H[i])//S[i])
     a.sort()
     for i in range(N):
-        if i > a[i]: return False
-
+        if a[i] < i: return False
     return True
 
 N = int(input())
-baloon = [list(map(int, input().split())) for _ in range(N)]
+H = []
+S = []
+for _ in range(N):
+    h, s = map(int, input().split())
+    H.append(h)
+    S.append(s)
 
 ng = 0
-ok = 10**18
-while abs(ok - ng) > 1:
-    mid = (ok + ng) // 2
-    if check(mid):
+ok = max(H)+max(S)*(N-1)
+while abs(ok-ng) > 1:
+    mid = (ok+ng)//2
+    if is_ok(mid):
         ok = mid
     else:
         ng = mid
-
 print(ok)
