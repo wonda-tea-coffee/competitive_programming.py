563fbd71d6135cd316dee0bb456c7e02e0f89e29
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-17 19:34:06 +0900

    refactor

--- aoj/ALDS1_10_B.py
+++ aoj/ALDS1_10_B.py
@@ -6,8 +6,8 @@ dp = [[float("inf")]*N for _ in range(N)]
 for i in range(N):
     dp[i][i] = 0
 
-for le in range(1, len(M)):
-    for i in range(len(M)-le):
+for le in range(1, N):
+    for i in range(N-le):
         j = i + le
         for k in range(i, j):
             dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + M[i][0]*M[k][1]*M[j][1])
