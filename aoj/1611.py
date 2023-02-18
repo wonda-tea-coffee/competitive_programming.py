3dd866c396700cfe9d4c9e86306b8d3cd9091622
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-19 02:19:40 +0900

    ACCCCCCCCCCCCCCCCCCCCCCCCCCCC

--- aoj/1611.py
+++ aoj/1611.py
@@ -1,20 +1,14 @@
-# オリジナル
-# https://drken1215.hatenablog.com/entry/2020/03/10/160500
-
 def solve():
-    dp = [[-1]*(N+1) for _ in range(N+1)]
-    for i in range(N+1):
-        dp[i][i] = 0
-        if i < N: dp[i][i+1] = 0
+    dp = [[0]*(N+1) for _ in range(N+1)]
 
     for bet in range(2, N+1):
         for i in range(N-bet+1):
             j = i + bet
             if dp[i+1][j-1] == j-i-2:
-                if abs(w[i] - w[j-1]) <= 1:
-                    dp[i][j] = max(dp[i][j], j-i)
+                if abs(W[i] - W[j-1]) < 2:
+                    dp[i][j] = j-i
                 else:
-                    dp[i][j] = max(dp[i][j], j-i-2)
+                    dp[i][j] = j-i-2
             for k in range(i+1, j):
                 dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j])
 
@@ -23,9 +17,5 @@ def solve():
 while True:
     N = int(input())
     if N == 0: break
-    w = list(map(int, input().split()))
-<<<<<<< HEAD
-=======
-    dp = [[-1]*N for _ in range(N)]
->>>>>>> e708bb6c2b2970895c2832ec5cf102a493704ba5
+    W = list(map(int, input().split()))
     print(solve())
