424ebaab537684cf0743b3768e6016f23cea0685
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-19 01:32:02 +0900

    solve

--- atcoder/joi2015ho_b.py
+++ atcoder/joi2015ho_b.py
@@ -1,39 +1,35 @@
-# オリジナル
-# https://at274.hatenablog.com/entry/2020/04/09/224749
-
 import sys
 sys.setrecursionlimit(1000000)
 
 N = int(input())
 A = [int(input()) for _ in range(N)]
-dp = [[0]*N for _ in range(N)]
+dp = [[-1]*N for _ in range(N)]
 
-def dfs(i, j, picked):
-    if dp[i][j] > 0: return dp[i][j]
+def dfs(i, j, turn):
+    if dp[i][j] >= 0: return dp[i][j]
 
     if i == j:
-        if picked == 0:
+        if turn:
             dp[i][j] = A[i]
-            return dp[i][j]
+            return A[i]
         else:
             return 0
 
+    ret = 0
     di, dj = (i + 1) % N, j - 1 if j - 1 >= 0 else N - 1
-    if picked == 0:
-        ret = max(dfs(di, j, 1) + A[i], dfs(i, dj, 1) + A[j])
+    if turn:
+        ret = max(dfs(di, j, not turn) + A[i], dfs(i, dj, not turn) + A[j])
     else:
         if A[i] > A[j]:
-            ret = dfs(di, j, 0)
+            ret = dfs(di, j, not turn)
         else:
-            ret = dfs(i, dj, 0)
+            ret = dfs(i, dj, not turn)
 
     dp[i][j] = ret
     return ret
 
 ans = 0
 for x in range(N):
-    s = (x + 1) % N
-    e = x - 1 if x - 1 >= 0 else N - 1
-    r = dfs(s, e, 1) + A[x]
-    ans = max(ans, r)
+    s, e = (x + 1) % N, x - 1 if x - 1 >= 0 else N - 1
+    ans = max(ans, dfs(s, e, False) + A[x])
 print(ans)
