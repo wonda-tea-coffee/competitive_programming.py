5f5cf952abb6a7f1819e2c9cc25c78d74236b99c
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-18 09:39:18 +0900

    ACCCCCCCCCCCC

--- aoj/ALDS1_10_B.py
+++ aoj/ALDS1_10_B.py
@@ -1,18 +1,19 @@
 N = int(input())
 M = [tuple(map(int, input().split())) for _ in range(N)]
 dp = [[float("inf")]*N for _ in range(N)]
-
-# 長さ0連鎖の演算量
 for i in range(N):
     dp[i][i] = 0
 
-# 長さ
-for length in range(1, N):
-    # 左端
-    for i in range(N-length):
-        # 右端
-        j = i + le
+for len in range(2, N+1):
+    # print("len:", len)
+    for i in range(N-len+1):
+        j = i+len-1
+        # print("  i ~ j = %d ~ %d" % (i, j))
         for k in range(i, j):
-            dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + M[i][0] * M[k][1] * M[j][1])
+            # [i, j] = min([i, k],[k+1, j])(k=1,2,...,j)
+            # print("      i ~ k = %d ~ %d" % (i, k))
+            # print("    k+1 ~ j = %d ~ %d" % (k+1, j))
+            dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + M[i][0]*M[k][1]*M[j][1])
 
+# print(dp)
 print(dp[0][-1])
