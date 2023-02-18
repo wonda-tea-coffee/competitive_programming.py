5e1e0e419c03e968067f1d3aecd2e63544440e54
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-19 02:55:06 +0900

    osouji

--- atcoder/s8pc_1_g.py
+++ atcoder/s8pc_1_g.py
@@ -11,29 +11,24 @@ for _ in range(M):
 INF = float("inf")
 dist_dp = [[INF]*N for i in range(1<<N)]
 num_dp = [[0]*N for i in range(1<<N)]
-
 dist_dp[0][0] = 0
 num_dp[0][0] = 1
 
 for bit in range(1<<N):
-    # jからkに向かうとする
-    for j, k in E:
-        # この状態に来ることができない
-        if num_dp[bit][j] == 0: continue
-        # kが既に訪問済み
-        if (bit>>k)&1 == 1: continue
+    for s, t in E:
+        if num_dp[bit][s] == 0: continue
+        if (bit>>t)&1 == 1: continue
 
-        d, time = E[(j, k)]
-        ndist = dist_dp[bit][j] + d
-        # 道が既に封鎖されている
-        if ndist > time: continue
+        d, time = E[(s, t)]
+        nd = dist_dp[bit][s] + d
+        if nd > time: continue
 
-        to = bit | (1<<k)
-        if dist_dp[to][k] == ndist:
-            num_dp[to][k] += num_dp[bit][j]
-        elif dist_dp[to][k] > ndist:
-            num_dp[to][k] = num_dp[bit][j]
-        dist_dp[to][k] = min(dist_dp[to][k], ndist)
+        to = bit | (1<<t)
+        if dist_dp[to][t] > nd:
+            num_dp[to][t] = num_dp[bit][s]
+        elif dist_dp[to][t] == nd:
+            num_dp[to][t] += num_dp[bit][s]
+        dist_dp[to][t] = min(dist_dp[to][t], nd)
 
 if dist_dp[-1][0] == INF:
     print("IMPOSSIBLE")
