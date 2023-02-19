f9a4f88bc8de54ebdc36af3340e149634ed8fd68
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-19 20:50:17 +0900

    模範解答をコピペ

--- atcoder/past202005_h.py
+++ atcoder/past202005_h.py
@@ -1,25 +1,41 @@
-N, L = map(int, input().split())
-x = set(map(int, input().split()))
-T1, T2, T3 = map(int, input().split())
-cost = [10**100] * (L + 1)
+# https://atcoder.jp/contests/past202005-open/submissions/20050113
 
+N, L = list(map(int, input().split()))
+X = list(map(int, input().split()))
+T1, T2, T3 = list(map(int, input().split()))
+
+# ハードルがある座標において True となるような配列
+H = [False]*(L+1)
+for x in X:
+    H[x] = True
+
+# cost[i]: 座標 i で行動を終了するまでの最小所要時間。
+# 非常に大きな値で初期化しておき、min を用いて更新する。
+cost = [10**100]*(L+1)
+
+# 初期条件
 cost[0] = 0
-for i in range(1, L + 1):
-    # 行動1
-    cost[i] = min(cost[i], cost[i - 1] + T1)
-    # 行動2
-    if i >= 2:
-        cost[i] = min(cost[i], cost[i - 2] + T1 + T2)
-    # 行動3
-    if i >= 4:
-        cost[i] = min(cost[i], cost[i - 4] + T1 + 3*T2)
 
-    if i in x:
+# 順番に求めていく
+for i in range(1, L+1):
+    # 行動 1
+    cost[i] = min(cost[i], cost[i-1] + T1)
+    # 行動 2
+    if i >= 2:
+        cost[i] = min(cost[i], cost[i-2] + T1 + T2)
+    # 行動 3
+    if i >= 4:
+        cost[i] = min(cost[i], cost[i-4] + T1 + 3*T2)
+    # 問題文中で
+    # 通り過ぎるときに座標xにハードルがある場合、その地点を通り過ぎるのに追加で～
+    # とある
+    # 次の行動をする際に加算するとよいが、通り過ぎる前に加算しても結果は変わらない
+    if H[i]:
         cost[i] += T3
 
+# 答えは座標 L にぴったり止まるか、座標 (L-3) ～ (L-1)からジャンプ中にゴールしたもの。
 ans = cost[L]
 for i in [L-3, L-2, L-1]:
     if i >= 0:
         ans = min(ans, cost[i] + T1//2 + T2*(2*(L-i)-1)//2)
-
 print(ans)
