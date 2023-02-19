a7b4dc2d2cee44999e00fc8801fa1f0559127450
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-19 19:25:19 +0900

    AC

--- atcoder/edp_a.py
+++ atcoder/edp_a.py
@@ -1,10 +1,9 @@
 N = int(input())
 h = list(map(int, input().split()))
-dp = [0] * N
-
+dp = [0]*N
+dp[0] = 0
 dp[1] = abs(h[1] - h[0])
 for i in range(2, N):
-    dp[i] = min(abs(h[i] - h[i - 1]) + dp[i - 1], abs(h[i] - h[i - 2]) + dp[i - 2])
+    dp[i] = min(dp[i-1] + abs(h[i] - h[i-1]), dp[i-2] + abs(h[i] - h[i-2]))
 
-# print(dp)
-print(dp[N - 1])
+print(dp[-1])
