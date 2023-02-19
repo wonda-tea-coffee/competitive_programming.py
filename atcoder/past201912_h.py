6032dfed111b7cabf0765ca5960111689f65ca30
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-19 11:42:57 +0900

    mata toita

--- atcoder/past201912_h.py
+++ atcoder/past201912_h.py
@@ -1,49 +1,46 @@
+def stock(i, cnt1, cnt2, cnt3):
+    sale = cnt1[i] + cnt3
+    if i % 2 == 0: sale += cnt2
+    return C[i] - sale
+
 N = int(input())
 C = list(map(int, input().split()))
+minall = min(C)
+minodd = min(C[::2])
 Q = int(input())
-m2 = min(C[::2])
-m3 = min(C)
-sum1 = [0] * N
-sum2 = 0
-sum3 = 0
+ans = 0
+cnt1 = [0]*N
+cnt2 = 0
+cnt3 = 0
 for i in range(Q):
-    S = list(map(int, input().split()))
-    # print("Q%i:" % (i + 1), S)
-    # print("C:", C)
-    if S[0] == 1:
-        x = S[1] - 1
-        a = S[2]
-        d = C[x] - sum1[x] - sum3
-        if x % 2 == 0:
-            d -= sum2
-        if d >= a:
-            d -= a
-            sum1[x] += a
-            if x % 2 == 0 and d < m2:
-                m2 = d
-            if d < m3:
-                m3 = d
-    else:
-        a = S[1]
-        if S[0] == 2:
-            if m2 >= a:
-                m2 -= a
-                sum2 += a
+    t, *q = map(int, input().split())
+    if t == 1:
+        x, a = q
+        x -= 1
 
-                if m3 > m2:
-                    m3 = m2
-        elif S[0] == 3:
-            if m3 >= a:
-                m2 -= a
-                m3 -= a
-                sum3 += a
+        s = stock(x, cnt1, cnt2, cnt3)
+        if s >= a:
+            ans += a
+            cnt1[x] += a
+            s -= a
+            if x % 2 == 0 and minodd > s:
+                minodd = s
+            if minall > s:
+                minall = s
+    elif t == 2:
+        a = q[0]
+        if minodd >= a:
+            cnt2 += a
+            ans += a * ((N+1)//2)
+            minodd -= a
+            if minall > minodd:
+                minall = minodd
+    elif t == 3:
+        a = q[0]
+        if minall >= a:
+            cnt3 += a
+            ans += a * N
+            minall -= a
+            minodd -= a
 
-    # print("m2:", m2)
-    # print("m3:", m3)
-    # print("sum1:", sum1)
-    # print("sum2:", sum2)
-    # print("sum3:", sum3)
-    # print("ans:", sum(sum1) + sum2 + sum3)
-
-# print("Ans:")
-print(sum(sum1) + sum2 * ((N + 1) // 2) + sum3 * N)
+print(ans)
