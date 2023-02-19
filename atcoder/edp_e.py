5c9d2f5f78fa13c2917dde8be4f42c84d171dc12
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-20 08:51:35 +0900

    solve

--- atcoder/edp_e.py
+++ atcoder/edp_e.py
@@ -1,23 +1,22 @@
 N, W = map(int, input().split())
-w = [None] * N
-v = [None] * N
+w = []
+v = []
 for i in range(N):
-    w[i], v[i] = map(int, input().split())
+    wi, vi = map(int, input().split())
+    w.append(wi)
+    v.append(vi)
 
-INF = 10 ** 18
-MAX = 10**3 * 100
-dp = [[INF] * (MAX+1) for _ in range(N+1)]
-dp[0][0] = 0
+M = 10**5+1
+dp = [W+1]*M
+dp[0] = 0
+for i in range(N):
+    ndp = list(dp)
+    for j in range(M):
+        if dp[j] + w[i] <= W:
+            ndp[j + v[i]] = min(ndp[j + v[i]], dp[j] + w[i])
+    dp = ndp
 
-for i in range(1, N+1):
-    for j in range(MAX+1):
-        if j - v[i-1] >= 0:
-            dp[i][j] = min(dp[i-1][j], dp[i-1][j-v[i-1]] + w[i-1])
-        else:
-            dp[i][j] = dp[i-1][j]
-
-for j in range(MAX, 0, -1):
-    # print(N, j)
-    if dp[N][j] <= W:
-        print(j)
-        break
+for i in range(M-1, -1, -1):
+    if dp[i] <= W:
+        print(i)
+        exit()
