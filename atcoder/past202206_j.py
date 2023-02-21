ac812b126394874c7123bf068bb758ff0f9f90c7
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-22 07:07:45 +0900

    hai

--- atcoder/past202206_j.py
+++ atcoder/past202206_j.py
@@ -1,45 +1,34 @@
+
 def is_leap(y):
-    if y % 400 == 0:
-        return True
-    elif y % 100 == 0:
-        return False
-    else:
-        return y % 4 == 0
+    if y % 400 == 0: return True
+    if y % 100 == 0: return False
+    return y % 4 == 0
 
-def next_day(y, m, d):
-    ld = [-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if is_leap(y):
-        ld[2] += 1
+def is_lastday(y, m, d):
+    if m == 2 and is_leap(y): return d == 29
+    return lastday[m] == d
 
-    if d == ld[m]:
-        d = 1
-        m += 1
-        if m == 13:
-            m = 1
-            y += 1
-    else:
-        d += 1
+def nextday(y, m, d):
+    if not is_lastday(y, m, d): return y, m, d+1
+    if m != 12: return y, m+1, 1
+    return y+1, 1, 1
 
-    return y, m, d
+lastday = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
 
 sy, sm, sd = map(int, input().split("-"))
 ty, tm, td = map(int, input().split("-"))
 
+iy, im, id = 2022, 1, 1
+week = 0 # 0, 1が土日とする
+while (iy, im, id) != (sy, sm, sd):
+    iy, im, id = nextday(iy, im, id)
+    week = (week + 1) % 7
+
 ans = 0
-ry, rm, rd = 2022, 1, 1
-w = 0
-while ry != sy or rm != sm or rd != sd:
-    ry, rm, rd = next_day(ry, rm, rd)
-    w = (w + 1) % 7
-
 while True:
-    # print(sy, sm, sd, w)
-    if w == 0 or w == 1:
-        ans += 1
-    if sy == ty and sm == tm and sd == td:
-        break
-
-    sy, sm, sd = next_day(sy, sm, sd)
-    w = (w + 1) % 7
+    if week == 0 or week == 1: ans += 1
+    if (iy, im, id) == (ty, tm, td): break
+    iy, im, id = nextday(iy, im, id)
+    week = (week + 1) % 7
 
 print(ans)
