8d9e8d8e5ee08203a5c2db2048df19632b135850
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-22 00:20:15 +0900

    AC

--- atcoder/past201912_k.py
+++ atcoder/past201912_k.py
@@ -1,37 +1,33 @@
-import sys
-sys.setrecursionlimit(1000000)
-
 N = int(input())
-R = -1
-edges = [[] for _ in range(N)]
+C = [[] for _ in range(N)]
+r = -1
 for i in range(N):
     p = int(input())
     if p == -1:
-        R = i
+        r = i
     else:
-        edges[p-1].append(i)
+        C[p-1].append(i)
+
+L = [-1] * N
+R = [-1] * N
+stk = [r]
+t = -1
+while stk:
+    v = stk.pop()
+    if v >= 0:
+        t += 1
+        L[v] = t
+        stk.append(~v)
+        for nv in C[v]:
+            stk.append(nv)
+    else:
+        v = ~v
+        R[v] = t
 
 Q = int(input())
-queries = [[] for _ in range(N)]
-for q in range(Q):
+for _ in range(Q):
     a, b = map(lambda x: int(x)-1, input().split())
-    queries[a].append((q, b))
-
-ans = [False]*Q
-boss = [False]*N
-
-def dfs(i):
-    for q, b in queries[i]:
-        ans[q] = boss[b]
-    boss[i] = True
-    for j in edges[i]:
-        dfs(j)
-    boss[i] = False
-
-dfs(R)
-
-for q in range(Q):
-    if ans[q]:
+    if L[b] < L[a] <= R[b]:
         print("Yes")
     else:
         print("No")
