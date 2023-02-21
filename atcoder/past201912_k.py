c6d0ee347785169eac9b0e2aedafc26d40b66d00
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-22 00:41:35 +0900

    AC

--- atcoder/past201912_k.py
+++ atcoder/past201912_k.py
@@ -1,36 +1,41 @@
 import sys
+sys.setrecursionlimit(1000000)
 input = lambda: sys.stdin.readline().rstrip()
 
 N = int(input())
-C = [[] for _ in range(N)]
+E = [[] for _ in range(N)]
 root = -1
 for i in range(N):
     p = int(input())
     if p == -1:
         root = i
     else:
-        C[p-1].append(i)
+        E[i].append(p-1)
+        E[p-1].append(i)
 
 L = [-1] * N
 R = [-1] * N
-stk = [root]
-t = -1
-while stk:
-    v = stk.pop()
-    if v >= 0:
-        t += 1
-        L[v] = t
-        stk.append(~v)
-        for nv in C[v]:
-            stk.append(nv)
-    else:
-        v = ~v
-        R[v] = t
+
+idx = 0
+def euler_tour(i, pa=-1):
+    global idx
+
+    L[i] = idx
+    idx += 1
+    for to in E[i]:
+        if to == pa: continue
+        euler_tour(to, i)
+    R[i] = idx
+
+euler_tour(root)
+
+# print("L:", L)
+# print("R:", R)
 
 Q = int(input())
 for _ in range(Q):
     a, b = map(lambda x: int(x)-1, input().split())
-    if L[b] < L[a] <= R[b]:
+    if L[b] <= L[a] and R[a] <= R[b]:
         print("Yes")
     else:
         print("No")
