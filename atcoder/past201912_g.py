2cf6c9364fed782748a62b1077eca935168c88eb
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-21 10:53:38 +0900

    uooooo

--- atcoder/past201912_g.py
+++ atcoder/past201912_g.py
@@ -1,38 +1,24 @@
-import sys
-sys.setrecursionlimit(1000000)
-
-def calc(g1, g2, g3):
-    ret = 0
-    for g in [g1, g2, g3]:
-        for i in range(len(g)):
-            for j in range(i+1, len(g)):
-                ret += h[(g[i], g[j])]
-    return ret
-
-def dfs(g1, g2, g3, n):
-    global ans
-
-    if n > N:
-        ans = max(ans, calc(g1, g2, g3))
-        return
-
-    for g in [g1, g2, g3]:
-        g.append(n)
-        dfs(g1, g2, g3, n+1)
-        g.pop()
+from itertools import combinations
 
 N = int(input())
-h = {}
-
-for i in range(1, N):
+A = [[0]*N for _ in range(N)]
+offset = 1
+for i in range(N-1):
     a = list(map(int, input().split()))
     for j in range(len(a)):
-        h[(i, i+j+1)] = a[j]
-        h[(i+j+1, i)] = a[j]
-h[(N, N-1)] = h[(N-1, N)]
+        A[i][j + offset] = a[j]
+        A[j + offset][i] = a[j]
+    offset += 1
 
-ans = -10**100
-
-dfs([], [], [], 1)
+ans = -10**10
+for bit in range(3**N):
+    g = [[] for _ in range(3)]
+    for i in range(N):
+        g[bit // 3**i % 3].append(i)
+    c = 0
+    for i in range(3):
+        for x, y in combinations(g[i], 2):
+            c += A[x][y]
+    ans = max(ans, c)
 
 print(ans)
