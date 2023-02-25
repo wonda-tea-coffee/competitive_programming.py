32c581ee2226f8e861efef4f6d286e8aea3d90fa
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-26 06:25:08 +0900

    maxが遅いのね

--- atcoder/past202206_h.py
+++ atcoder/past202206_h.py
@@ -1,7 +1,5 @@
 N, A, B = map(int, input().split())
-dp = [[-1]*(B+1) for _ in range(A+1)]
-dp[0][0] = 0
-ans = 0
+dp = [[0]*(B+1) for _ in range(A+1)]
 
 for _ in range(N):
     w, v = map(int, input().split())
@@ -9,17 +7,13 @@ for _ in range(N):
 
     for i in range(A+1):
         for j in range(B+1):
-            if dp[i][j] == -1: continue
-
             ndp[i][j] = max(ndp[i][j], dp[i][j])
             nv = dp[i][j] + v
             if i+w <= A:
                 ndp[i+w][j] = max(ndp[i+w][j], nv)
-                ans = max(ans, ndp[i+w][j])
             if j+w <= B:
                 ndp[i][j+w] = max(ndp[i][j+w], nv)
-                ans = max(ans, ndp[i][j+w])
 
     dp = ndp
 
-print(ans)
+print(dp[A][B])
