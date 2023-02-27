74bb61372a8fffb358e94eaf4cd10aef41a4f24b
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-27 20:32:21 +0900

    一旦

--- atcoder/tessoku_book_u.py
+++ atcoder/tessoku_book_u.py
@@ -1,34 +1,37 @@
-import sys
-sys.setrecursionlimit(1000000)
-
 N = int(input())
 P = [0]
 A = [0]
-for _ in range(N):
-    p, a = map(int, input().split())
-    P.append(p)
-    A.append(a)
-block = list(range(N+1))
+for i in range(1, N+1):
+	pi, ai = map(int, input().split())
+	P.append(pi)
+    A.append(ai)
 
-dp = [[-1]*(N+1) for _ in range(N+1)]
-def solve(i, j):
-    if dp[i][j] >= 0: return dp[i][j]
-    if i == j: return 0
+# 動的計画法（bet は r-l の値)
+dp = [[None]*(N+1) for _ in range(N+1)]
+dp[1][N] = 0
+for bet in range(N-1, -1, -1):
+	for l in range(1, N-bet+1):
+		r = l+bet
 
-    ret = 0
-    # ブロックP[i]が既に取り除かれているか
-    if P[i] < i or j < P[i]:
-        ret = solve(i+1, j)
-    else:
-        ret = solve(i+1, j) + A[i]
+		# score1 の値（l-1 番目のブロックを取り除くときの得点）を求める
+		score1 = 0
+		if l >= 2 and l <= P[l-1] <= r:
+			score1 = A[l-1]
 
-    # ブロックP[j]が既に取り除かれているか
-    if P[j] < i or j < P[j]:
-        ret = max(ret, solve(i, j-1))
-    else:
-        ret = max(ret, solve(i, j-1) + A[j])
+		# score2 の値（r+1 番目のブロックを取り除くときの得点）を求める
+		score2 = 0
+		if r <= N-1 and l <= P[r+1] <= r:
+			score2 = A[r+1]
 
-    dp[i][j] = ret
-    return ret
+		# dp[l][r] を求める
+		if l == 1:
+			dp[l][r] = dp[l][r+1] + score2
+		elif r == N:
+			dp[l][r] = dp[l-1][r] + score1
+		else:
+			dp[l][r] = max(dp[l-1][r] + score1, dp[l][r+1] + score2)
 
-print(solve(1, N))
+ans = 0
+for i in range(1, N+1):
+	ans = max(ans, dp[i][i])
+print(ans)
