c3f9b31413ac98fac9d28a23642c28d5ecbf90ce
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-27 17:14:05 +0900

    solve

--- atcoder/tessoku_book_s.py
+++ atcoder/tessoku_book_s.py
@@ -9,8 +9,9 @@ dp = [[0]*(W+1) for _ in range(N+1)]
 
 for i in range(N):
     for j in range(W+1):
-        dp[i+1][j] = max(dp[i+1][j], dp[i][j])
-        if j+w[i] <= W:
-            dp[i+1][j+w[i]] = max(dp[i+1][j+w[i]], dp[i][j]+v[i])
+        if j < w[i]:
+            dp[i+1][j] = dp[i][j]
+        else:
+            dp[i+1][j] = max(dp[i][j], dp[i][j-w[i]] + v[i])
 
 print(max(dp[-1]))
