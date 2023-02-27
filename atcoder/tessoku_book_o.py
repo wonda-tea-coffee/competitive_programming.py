c0b36ebf5b7420ca72811715c38c739e9e91b430
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-02-27 14:48:24 +0900

    かしこい

--- atcoder/tessoku_book_o.py
+++ atcoder/tessoku_book_o.py
@@ -1,11 +1,7 @@
-import bisect
-
 N = int(input())
 A = list(map(int, input().split()))
-B = sorted(set(A))
-
-ans = []
-for ai in A:
-    i = bisect.bisect_left(B, ai)
-    ans.append(i+1)
-print(*ans)
+d = {}
+for i, a in enumerate(sorted({*A}), 1):
+    d[a] = i
+for a in A:
+    print(d[a])
