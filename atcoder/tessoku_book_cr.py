930fcb42054ef2d41e333964db0b7513bf802f94
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-03-01 20:46:35 +0900

    AC

--- atcoder/tessoku_book_cr.py
+++ atcoder/tessoku_book_cr.py
@@ -1,23 +1,20 @@
-N, W = map(int, input().split())
-w = [0]
-v = [0]
-for _ in range(N):
-    wi, vi = map(int, input().split())
-    w.append(wi)
-    v.append(vi)
+import sys
+input = lambda: sys.stdin.readline().rstrip()
 
-M = 100000
+N, W = map(int, input().split())
+M = 10**5
 dp = [[10**15]*(M+1) for _ in range(N+1)]
 dp[0][0] = 0
 
-for i in range(1, N+1):
-	for j in range(M+1):
-		if j < v[i]:
-			dp[i][j] = dp[i-1][j]
-		else:
-			dp[i][j] = min(dp[i-1][j], dp[i-1][j-v[i]]+w[i])
+for i in range(N):
+    w, v = map(int, input().split())
+    for j in range(M+1):
+        if j >= v:
+            dp[i+1][j] = min(dp[i][j], dp[i][j-v]+w)
+        else:
+            dp[i+1][j] = dp[i][j]
 
 for i in range(M, -1, -1):
-	if dp[N][i] <= W:
-		print(i)
-		exit()
+    if dp[N][i] <= W:
+        print(i)
+        exit()
