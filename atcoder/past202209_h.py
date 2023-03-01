e641353ba4cb6d5cb6ab48d637e4c0f3d368d5a9
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-03-01 20:54:16 +0900

    AC

--- atcoder/past202209_h.py
+++ atcoder/past202209_h.py
@@ -5,16 +5,16 @@ N, X = map(int, input().split())
 dp = [[(0, 10**9)]*(X+1) for _ in range(N+1)]
 
 for i in range(N):
-	A, B, C = map(int, input().split())
-	for j in range(X+1):
-		if j+B <= X:
-			gold, silver = dp[i][j+B]
-			dp[i+1][j] = max(dp[i][j], (gold+C, silver-A))
-		else:
-			dp[i+1][j] = dp[i][j]
+    A, B, C = map(int, input().split())
+    for j in range(X+1):
+        if j+B <= X:
+            gold, silver = dp[i][j+B]
+            dp[i+1][j] = max(dp[i][j], (gold+C, silver-A))
+        else:
+            dp[i+1][j] = dp[i][j]
 
 ans = (0, 0, 0)
 for i in range(X+1):
-    a, b = dp[-1][i]
-    ans = max(ans, (a, b, i))
+    gold, silver = dp[N][i]
+    ans = max(ans, (gold, silver, i))
 print(*ans)
