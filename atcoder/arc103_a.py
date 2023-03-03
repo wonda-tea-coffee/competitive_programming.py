ae65ad23aa1a5feda2e9bcc9fa2f2cd514a43b99
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-03-03 11:59:27 +0900

    benri

--- atcoder/arc103_a.py
+++ atcoder/arc103_a.py
@@ -2,24 +2,10 @@ from collections import Counter
 
 n = int(input())
 v = list(map(int, input().split()))
+c1 = Counter(v[::2]).most_common() + [(0, 0)]
+c2 = Counter(v[1::2]).most_common() + [(0, 0)]
 
-r = []
-
-c1 = Counter(v[::2])
-q1 = []
-for ci1 in c1:
-    q1.append((c1[ci1], ci1))
-q1.append((0, 0))
-q1.sort(reverse=True)
-
-c2 = Counter(v[1::2])
-q2 = []
-for ci2 in c2:
-    q2.append((c2[ci2], ci2))
-q2.append((0, 0))
-q2.sort(reverse=True)
-
-if q1[0][1] == q2[0][1]:
-    print(min(n//2 - q1[0][0] + n//2 - q2[1][0], n//2 - q1[1][0] + n//2 - q2[0][0]))
+if c1[0][0] == c2[0][0]:
+    print(min(n - c1[0][1] - c2[1][1], n - c1[1][1] - c2[0][1]))
 else:
-    print(n//2 - q1[0][0] + n//2 - q2[0][0])
+    print(n - c1[0][1] - c2[0][1])
