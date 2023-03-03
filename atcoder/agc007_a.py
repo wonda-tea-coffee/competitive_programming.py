e564a62e2b0846ba13b23369e2ba8fa0bc89650f
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-03-03 13:56:47 +0900

    AC

--- atcoder/agc007_a.py
+++ atcoder/agc007_a.py
@@ -1,19 +1,5 @@
-from collections import deque
-
 H, W = map(int, input().split())
-A = [input() for _ in range(H)]
+S = [input() for _ in range(H)]
 
-for i in range(H):
-    if A.count("#.#") > 0:
-        print("Impossible")
-        exit()
-
-r = 0
-for i in range(1, H):
-    ri = A[i-1].rindex("#")
-    li = A[i].index("#")
-    if li < ri:
-        print("Impossible")
-        exit()
-
-print("Possible")
+c = sum(map(lambda x: x.count("#"), S))
+print("Possible" if c == H+W-1 else "Impossible")
