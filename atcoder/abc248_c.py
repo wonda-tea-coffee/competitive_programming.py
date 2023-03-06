ac89a5bc79dd70bc57c14d4917fb56a4fc6ed776
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-03-06 09:16:13 +0900

    DP

--- atcoder/abc248_c.py
+++ atcoder/abc248_c.py
@@ -1,28 +1,13 @@
-import sys
-sys.setrecursionlimit(10000000)
-
-memo = {}
-def solve(n, k):
-    if (n, k) in memo: return memo[(n, k)]
-
-    if n == 1:
-        if 1 <= k <= M:
-            return 1
-        else:
-            return 0
-
-    ret = 0
-    for i in range(1, M+1):
-        if i > k: break
-        ret = (ret + solve(n-1, k-i)) % MOD
-
-    memo[(n, k)] = ret
-    return ret
-
 N, M, K = map(int, input().split())
 MOD = 998244353
+dp = [[0]*(K+1) for _ in range(N+1)]
+dp[0][0] = 1
 
-ans = 0
-for k in range(1, K+1):
-    ans = (ans + solve(N, k)) % MOD
-print(ans)
+for i in range(N):
+    for j in range(K+1):
+        for k in range(1, min(j, M)+1):
+            dp[i+1][j] += dp[i][j-k]
+
+        dp[i+1][j] %= MOD
+
+print(sum(dp[-1]) % MOD)
