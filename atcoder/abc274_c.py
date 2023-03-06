ec0d0f7a8aa4f9b4f1ebe758537529a475a3bb55
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-03-06 16:09:33 +0900

    hai

--- atcoder/abc274_c.py
+++ atcoder/abc274_c.py
@@ -1,2 +1,21 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
 N = int(input())
-A = list(map(int, input().split()))
+A = [0] + list(map(int, input().split()))
+
+memo = {}
+def solve(k):
+    if k in memo: return memo[k]
+    if k == 1: return 0
+
+    if k % 2 == 0:
+        ret = solve(A[k//2]) + 1
+    else:
+        ret = solve(A[(k-1)//2]) + 1
+
+    memo[k] = ret
+    return ret
+
+for k in range(1, 2*N+2):
+    print(solve(k))
