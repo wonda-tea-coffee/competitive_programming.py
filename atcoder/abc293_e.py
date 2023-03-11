c6002d5670c8e0b0dd06feda923dcd7bd1c76eb4
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-03-12 08:23:09 +0900

    完全に理解した

--- atcoder/abc293_e.py
+++ atcoder/abc293_e.py
@@ -4,7 +4,7 @@ A, X, M = map(int, input().split())
 
 @lru_cache(maxsize=None)
 def f(x):
-    if x <= 1: return x
+    if x == 0: return 0
     elif x % 2 == 0:
         r = x//2
         return ((1 + pow(A,r,M)) * f(r)) % M
