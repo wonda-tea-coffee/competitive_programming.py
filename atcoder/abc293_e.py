22c2ad6da479c1322873ea0f35974625eca5b101
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-03-12 08:13:15 +0900

    AC

--- atcoder/abc293_e.py
+++ atcoder/abc293_e.py
@@ -1,5 +1,14 @@
+from functools import lru_cache
+
 A, X, M = map(int, input().split())
-if A == 1:
-    print(X % M)
-else:
-    print((pow(A, X, M*(A-1))-1) // (A-1))
+
+@lru_cache(maxsize=None)
+def f(x):
+    if x == 0:
+        return 0
+    elif x % 2 == 0:
+        return f(x//2)*(1+pow(A,x//2,M)) % M
+    else:
+        return (f(x-1)*A + 1) % M
+
+print(f(X))
